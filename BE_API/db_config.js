df040b492523514254cd08fb7aecae3ac48d4673
huynhthaikhoa <23050083@student.bdu.edu.vn>
2025-12-10 12:13:41 +0700

    Update database credentials in db_config.js

--- BE_API/db_config.js
+++ BE_API/db_config.js
@@ -3,10 +3,11 @@ module.exports = {
     // **QUAN TRỌNG: Thay thế bằng Endpoint RDS MySQL của bạn**
     host: 'tuyensinhdb.czk86e4ggm4t.ap-southeast-2.rds.amazonaws.com', 
     user: 'admin',      // User name bạn tạo khi tạo RDS
-    password: 'Hai24122004!', // Password bạn tạo khi tạo RDS
-    database: 'tuyensinhdb', // Tên Database
+    password: '0865575090', // Password bạn tạo khi tạo RDS
+    database: 'TuyensinhBDU', // Tên Database
     port: 3306
 };
+
 
 
 
